f4b46fafb9cb56a778fba6cddaa31d0e2c507fd3
jjia <972931271@qq.com>
2020-11-15 21:09:59 +0100

    set default data_folder

--- set_args.py
+++ set_args.py
@@ -3,12 +3,12 @@
 # @Author  : Jingnan
 # @Email   : jiajingnan2222@gmail.com
 import argparse
-ex_id = 0
+ex_id = 8
 ex_dir = "ex"+str(ex_id)
 
 parser = argparse.ArgumentParser(description="Run a basic UNet segmentation baseline.")
-parser.add_argument("--mode", type=str, default="infer", choices=("train", "infer"), help="mode of workflow")
-parser.add_argument("--data_folder", type=str, default="", help="training data folder")
+parser.add_argument("--mode", type=str, default="train", choices=("train", "infer"), help="mode of workflow")
+parser.add_argument("--data_folder", type=str, default="COVID-19-20_v2/Train", help="training data folder")
 parser.add_argument("--model_folder", type=str, default="models/"+ex_dir, help="model folder")
 parser.add_argument("--result_folder", type=str, default="results/"+ex_dir, help="model folder")
 
