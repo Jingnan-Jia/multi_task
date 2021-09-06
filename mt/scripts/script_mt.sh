b365dd3d7fba26f707a210b421af1f6bd05d5304
jjia <972931271@qq.com>
2021-09-06 23:08:28 +0200

    create results dir if not exist

--- mt/scripts/script_mt.sh
+++ mt/scripts/script_mt.sh
@@ -14,16 +14,18 @@ conda activate py38
 
 job_id=$SLURM_JOB_ID
 slurm_dir=results/slurmlogs
+mkdir -p ${slurm_dir}  # create dir if not exist
 
 scontrol write batch_script ${job_id} ${slurm_dir}/slurm-${job_id}_args.sh
 cp ../mymodules/set_args_mtnet.py ${slurm_dir}/slurm-${job_id}_set_args_mtnet.py  # backup setting
 
 
-time=$(date +%y_%m_%d_%H_%M_%S)  # there must not be any space before and after =
+#time=$(date +%y_%m_%d_%H_%M_%S)  # there must not be any space before and after =
 
-export PYTHONPATH="${PYTHONPATH}:/data/jjia/jjnutils/jjnutils"
+#export PYTHONPATH="${PYTHONPATH}:/data/jjia/jjnutils/jjnutils"
 
-idx=0; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u train_mtnet.py 2>${slurm_dir}/slurm-${job_id}_$idx.err 1>${slurm_dir}/slurm-${job_id}_$idx.out --net_names="net_airway_itgt-net_recon" --mode="train" --main_net_name="net_airway_itgt" --ad_lr=0 --ratio_norm_gradients=0 &
+idx=0; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u train_mtnet.py 2>${slurm_dir}/slurm-${job_id}_$idx.err 1>${slurm_dir}/slurm-${job_id}_$idx.out --loss='dice' &
+idx=1; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u train_mtnet.py 2>${slurm_dir}/slurm-${job_id}_$idx.err 1>${slurm_dir}/slurm-${job_id}_$idx.out --loss='CE' &
 
 wait
 
