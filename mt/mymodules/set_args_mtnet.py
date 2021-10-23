a237e100951a417ce0c0129cd7aac2639c79d5ae
jjia <972931271@qq.com>
2021-10-23 13:16:15 +0200

    set training

--- mt/mymodules/set_args_mtnet.py
+++ mt/mymodules/set_args_mtnet.py
@@ -11,7 +11,7 @@ def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Run multi-task UNet segmentation.")
 
     # model_choices = ["net_lobe", "net_vessel", "net_recon", "net_lesion"]
-    parser.add_argument('--mode', choices=("train", "infer"), help='main model ', type=str, default='infer')
+    parser.add_argument('--mode', choices=("train", "infer"), help='main model ', type=str, default='train')
     parser.add_argument('--train_mode', choices=("stepbystep", "onetime"), help='main model ', type=str, default='stepbystep')
     parser.add_argument('--net_names', help='model names', type=str,
                         choices=("net_lobe_itgt", "net_lobe",
@@ -25,7 +25,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument('--data_path', help='change main task will change it ', type=str, default='data_ori_space')  # data_xy77_z5
     parser.add_argument('--loss', help='loss function', choices=('dice', 'CE', 'dice_CE', 'weighted_dice',
                                                                  'weighted_CE_fnfp', 'weighted_CE_fn'),
-                        type=str, default='weighted_CE_fn')  # balanced_dice, balanced_ce
+                        type=str, default='dice')  # balanced_dice, balanced_ce
 
     parser.add_argument('--pps', help='patches_per_scan', type=int, default=10)
     parser.add_argument('--amp', help='amp', type=bool, default=True)
@@ -51,8 +51,8 @@ def get_args() -> argparse.Namespace:
 
     # learning rate, normally lr of main model is greater than others
     parser.add_argument('-lr_ls', '--lr_ls', type=float, default=0.00001)
-    parser.add_argument('-lr_lb', '--lr_lb', type=float, default=0.00001)
-    parser.add_argument('-lr_vs', '--lr_vs', type=float, default=0.0001)
+    parser.add_argument('-lr_lb', '--lr_lb', type=float, default=0.0001)
+    parser.add_argument('-lr_vs', '--lr_vs', type=float, default=0.00001)
     parser.add_argument('-lr_aw', '--lr_aw', type=float, default=0.00001)
     parser.add_argument('-lr_lu', '--lr_lu', type=float, default=0.00001)
     parser.add_argument('-lr_rc', '--lr_rc', type=float, default=0.00001)
@@ -92,7 +92,7 @@ def get_args() -> argparse.Namespace:
 
     # name of loaded trained model for single-task net
     parser.add_argument('--ld_ls', type=str, default='')
-    parser.add_argument('--ld_lb', type=str, default='1630962918_874')
+    parser.add_argument('--ld_lb', type=str, default='')  # 1631188707_500
     parser.add_argument('--ld_vs', type=str, default='')
     parser.add_argument('--ld_aw', type=str, default='')
     parser.add_argument('--ld_lu', type=str, default='')
