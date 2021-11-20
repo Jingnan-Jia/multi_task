0129e51cd121a7a8cd4a24bd7a86ca2c9164a233
jjia <972931271@qq.com>
2021-11-21 00:32:17 +0100

    new experiments

--- mt/scripts/generate_lola11_style_fissure.py
+++ mt/scripts/generate_lola11_style_fissure.py
@@ -20,27 +20,30 @@ def gnt_lola11_style_fissure(gdth_folder, pred_folder):
             if np.any(gdth_slice):
                 fissure_points[idx] = pred_slice
         fissure_points = np.rollaxis(fissure_points, 1, 0)  # roll axis back
-        save_itk(pred_folder + "/" + os.path.basename(gdth_file), fissure_points, ori, sp)
+        save_dir = pred_folder + "/points"
+        if not os.path.isdir(save_dir):
+            os.mkdir(save_dir)
+        save_itk(save_dir + '/' + os.path.basename(gdth_file), fissure_points, ori, sp)
 
 
 
 
     # lobe_abs_dir = "/data/jjia/monai/models/lobe/1611152191_299/infer_pred/lola11/to_submit"
 dir_1 = "/data/jjia/multi_task/mt/scripts/results/lobe/"
-dir_2 = "/infer_pred/lola11"
+dir_2 = "/infer_pred/lola11/largest_connected"
 id_list = [
-            "1635031568_650",
-            "1635031142_572",
-            "1635031142_162",
-            "1635031115_575",
-            "1635031115_596",
-            "1635031365_299",
-            "1635031365_967",
-            # "1635031434_249",
-            # "1635031434_34",
-            # "1635031521_103",
-            # "1635031521_727",
-            # "16",
+            # "57",
+            # "58",
+            # "59",
+            # "60",
+            # "61",
+            # "62",
+            # "63",
+            # "64",
+            # "65",
+            # "66",
+            # "67",
+            "88",
             ]
 dir_list = [dir_1 + id + dir_2 for id in id_list]
 gdth_folder = "/data/jjia/multi_task/mt/scripts/data/data_ori_space/lola11"
