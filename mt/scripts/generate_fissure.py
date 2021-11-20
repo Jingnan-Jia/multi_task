5daa569a88419839ee3aa44b28665f0638fd91ba
jjia <972931271@qq.com>
2021-11-21 00:33:21 +0100

    new experiments

--- mt/scripts/generate_fissure.py
+++ mt/scripts/generate_fissure.py
@@ -1,27 +1,36 @@
 import sys
 
 sys.path.append("../..")
+import argparse
 
 from mt.mymodules.generate_fissure_from_masks import gntFissure, gntLung
 
+parser = argparse.ArgumentParser(description="Run multi-task UNet segmentation.")
+parser.add_argument('--ex', help='ex id ', type=str, default='57')
+args = parser.parse_args()
+
 # lobe_abs_dir = "/data/jjia/monai/models/lobe/1611152191_299/infer_pred/lola11/to_submit"
 dir_1 = "/data/jjia/multi_task/mt/scripts/results/lobe/"
-dir_2 = "/infer_pred/lola11"
+dir_2 = "/infer_pred/GLUCOLD"
 id_list = [
-            "1635031568_650",
-            "1635031142_572",
-            "1635031142_162",
-            "1635031115_575",
-            "1635031115_596",
-            "1635031365_299",
-            "1635031365_967",
-            # "1635031434_249",
-            # "1635031434_34",
-            # "1635031521_103",
-            # "1635031521_727",
-            # "16",
+    # args.ex
+            # "57",
+            # "58",
+            # "59",
+            # "60",
+            # "61",
+            # "62",
+            # "63",
+            # "64",
+            # "65",
+            # "66",
+            # "67",
+            "88",
             ]
 dir_list = [dir_1 + id + dir_2 for id in id_list]
+
+# dir_list = ["/data/jjia/multi_task/mt/scripts/data/data_ori_space/lobe/test_data"]
 for dir in dir_list:
+    print(dir)
     gntFissure(dir, radiusValue=1, workers=5, labels=[1,2,3,4,5])
     # gntLung(dir, workers=5)
