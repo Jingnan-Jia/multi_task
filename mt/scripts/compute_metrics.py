05ff800c28301ce98c253a94036b51981af72819
jjia <972931271@qq.com>
2021-11-21 00:33:07 +0100

    new experiments

--- mt/scripts/compute_metrics.py
+++ mt/scripts/compute_metrics.py
@@ -2,27 +2,34 @@ import os
 import seg_metrics.seg_metrics as sg
 
 
-dir1 = "/data/jjia/multi_task/mt/scripts/results/lobe/"
-dir2 = "/infer_pred/lola11"
+dir1 = "/data/jjia/multi_task/mt/scripts/results/av/"
+dir2 = "/infer_pred/SYSU"
 ex_id_ls = [
-    # "1635031568_650",
-    #          "1635031142_572",
-    #          "1635031142_162",
-    #          "1635031115_575",
-    #          "1635031115_596",
-             "1635031365_299",
-             "1635031365_967"
+    # "57",
+    # "58",
+    # "59",
+    # "60",
+    # "61",
+    # "62",
+    # "63",
+    # "64",
+    # "18",
+    # "19",
+    "16",
+    # "21",
              ]
 
-gdth_path = "/data/jjia/multi_task/mt/scripts/data/data_ori_space/lola11"
-
+gdth_path = "/data/jjia/multi_task/mt/scripts/data/data_ori_space/av"
+print(ex_id_ls)
 for ex_id in ex_id_ls:
-    pred_path = dir1 + ex_id + dir2
-    csv_file = os.path.join(pred_path, 'metrics_on_fissure.csv')
 
-    metrics = sg.write_metrics(labels=[1],  # exclude background
+    pred_path = dir1 + ex_id + dir2
+    csv_file = os.path.join(pred_path, 'metrics_on_av.csv')
+
+    metrics = sg.write_metrics(labels=[1,2],  # exclude background
                                gdth_path=gdth_path,
                                pred_path=pred_path,
                                csv_file=csv_file,
-                               metrics=['hd', 'hd95', 'msd'])
+                               metrics=['dice', 'jaccard', 'precision', 'recall', 'fpr', 'fnr', 'vs', 'hd', 'hd95', 'msd', 'mdsd',
+                         'stdsd'])
     print('metrics: ', metrics)
