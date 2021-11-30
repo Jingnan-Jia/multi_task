1cbf37bc9d1f0ba4e5ef55d919f518d2488fb94a
jjia <972931271@qq.com>
2021-11-30 13:06:39 +0100

    add av plots

--- mt/scripts/comparison_plot.py
+++ mt/scripts/comparison_plot.py
@@ -13,7 +13,7 @@ def load_data(ex_dt, fissure_or_lobe='lobe', task='lobe', dataset='GLUCOLD', lar
     elif task == 'lobe':
         if largest_connected:
             if dataset=='lola11':
-                path_after = "/infer_pred/" + dataset + "/largest_connected/points/metrics_on_fissure_points.csv"
+                path_after = "/infer_pred/" + dataset + "/largest_connected/points/metrics_on_fissure.csv"
             else:
                 path_after = "/infer_pred/" + dataset + "/largest_connected/metrics_on_" + fissure_or_lobe + ".csv"
         else:
@@ -62,20 +62,54 @@ def merge_pd_by(data_df: pd.DataFrame, by: str):
 
 
 if __name__ == "__main__":
-    task = 'av'  # av
-    dataset = 'SYSU' # 'lola11'
+    task = 'av'  # av or 'lobe'
+    dataset = 'SYSU' # 'lola11', or 'GLUCOLD', or 'GLUCOLD_remove_fissure' or 'SYSU'
     fissure_or_lobe = 'lobe'  # 'fissure', 'lobe', 'fissure_points'
-    largest_connected = False
+    largest_connected = True
     different_tr_nb = False
+    tr_nb = 7 # 1,3,8, 16
+
     if task == 'av':
-        ex_dt = {
-            'av, tr_nb=7, av+rc': '21',
-            'av, tr_nb=1, av+rc': '18',
-            'av, tr_nb=1, av+rc+aw': '17',
-            # 'av, tr_nb=3, av+rc+aw': '16',
-            # 'av, tr_nb=3, av+aw': '20',
-            # 'av, tr_nb=3, av': '19',
-        }
+        if tr_nb == 1:
+            ex_dt = {
+                'av': '65',
+                #
+                # 'av, ad_lr=0, av+rc': '62',
+                # 'av, ad_lr=0, av+aw': '89',
+                # 'av, ad_lr=0, av+rc+aw': '103',
+
+                'av, ad_lr=0.1, av+rc': '71',
+                'av, ad_lr=0.1, av+aw': '92',
+                'av, ad_lr=0.1, av+rc+aw': '110',
+            }
+        if tr_nb == 3:
+            ex_dt = {
+                'av': '64',
+
+                # 'av, ad_lr=0, av+rc': '63',
+                # 'av, ad_lr=0, av+aw': '90',
+                # 'av, ad_lr=0, av+rc+aw': '104',
+
+                'av, ad_lr=0.1, av+rc': '70',
+                'av, ad_lr=0.1, av+aw': '91',
+                'av, ad_lr=0.1, av+rc+aw': '109',
+            }
+        if tr_nb == 7:
+            ex_dt = {
+                'av': '78',
+
+                'av, ad_lr=0.1, av+rc': '82',
+                'av, ad_lr=0.1, av+aw': '81',
+                # 'av, ad_lr=0.1, av+rc+aw': '108',
+            }
+        # ex_dt = {
+        #     'av, tr_nb=7, av+rc': '21',
+        #     'av, tr_nb=1, av+rc': '18',
+        #     'av, tr_nb=1, av+rc+aw': '17',
+        #     # 'av, tr_nb=3, av+rc+aw': '16',
+        #     # 'av, tr_nb=3, av+aw': '20',
+        #     # 'av, tr_nb=3, av': '19',
+        # }
     else:
         if different_tr_nb:
             ex_dt = {'lb, tr_nb=1': '64',
@@ -83,18 +117,50 @@ if __name__ == "__main__":
                      'lb, tr_nb=8': '66',
                      'lb, tr_nb=16': '65'}
         else:
-            ex_dt = {
-                # 'baseline_old': '16',
-                'lb': '66',
-                'lb+vs': '60',
-                'lb+rc': '59',
-                'lb+vs, ad_lr=0.1': '57',
-                'lb+rc, ad_lr=0.1': '58',
-                'lb+vs, ad_gd=0.1': '62',
-                'lb+rc, ad_gd=0.1': '63',
-                'lb+vs+rc, ad_lr=0.1': '67',
-                'lb+vs+rc, ad_gd=0.1': '68',
-            }
+            if tr_nb==1:
+                ex_dt = {
+                    # 'baseline_old': '16',
+                    'lb': '169',
+
+                    'lb+av, ad_lr=0': '107',  # ad_lr=0
+                    'lb+vs, ad_lr=0': '109',
+                    'lb+rc, ad_lr=0': '112',
+
+                    # 'lb+vs, ad_lr=0.1': '117',
+                    # 'lb+rc, ad_lr=0.1': '114',
+                    # 'lb+av, ad_lr=0.1': '116',
+                    #
+                    # 'lb+av+rc, ad_lr=0.1': '120',
+                }
+            elif tr_nb==3:
+                ex_dt = {
+                    # 'baseline_old': '16',
+                    'lb': '171',
+                    'lb+av, ad_lr=0': '108',
+                    'lb+vs, ad_lr=0': '110',
+                    'lb+rc, ad_lr=0': '111',
+
+                    # 'lb+vs, ad_lr=0.1': '118',
+                    # 'lb+rc, ad_lr=0.1': '113',
+                    # 'lb+av, ad_lr=0.1': '115',
+                    #
+                    # 'lb+av+rc, ad_lr=0.1': '119',
+                }
+            elif tr_nb==16:
+                ex_dt = {
+                    # 'baseline_old': '16',
+                    'lb': '170',
+                    # 'lb+vs': '60',
+                    # 'lb+rc': '59',
+                    # 'lb+vs, ad_lr=0.1': '57',
+                    # 'lb+rc, ad_lr=0.1': '58',
+                    'lb+vs, ad_gd=0.1': '62',
+                    'lb+rc, ad_gd=0.1': '63',
+                    # 'lb+vs+rc, ad_lr=0.1': '67',
+                    'lb+vs+rc, ad_gd=0.1': '68',
+                }
+            else:
+                raise Exception(f"wrong tr_nb {tr_nb}")
         # ex_dt = {
         #     'lb, tr_nb=16, coarse gdth': '88',
         #     'lb, tr_nb=16, fine gdth': '65',
@@ -125,9 +191,9 @@ if __name__ == "__main__":
             new_data_pd_list.append({k: new_v})
     # 'dice', 'fpr', 'fnr', for lobe
     if task=='lobe':
-        title_prefix = task + ' segmentation (tr_nb=16) ' + '_'.join([dataset, fissure_or_lobe])
+        title_prefix = task + f' segmentation (tr_nb={tr_nb}) ' + '_'.join([dataset, fissure_or_lobe])
     elif task=='av':
         title_prefix = task + ' segmentation ' + '_'.join([dataset])
 
-    comparison_plot(new_data_pd_list, metrics=['dice', 'fpr', 'fnr', 'hd95', 'msd'],
+    comparison_plot(new_data_pd_list, metrics=['dice', 'hd95', 'msd'],
                     title_prefix=title_prefix)  # 'hd95','hd', 'msd' for fissure metrics
